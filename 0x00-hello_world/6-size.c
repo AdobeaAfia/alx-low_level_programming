e2bdb97a7cfce61ec03ac63efc551ca907e03eed
AdobeaAfia <adobea_afia@yahoo.com>
2023-10-05 17:00:01 -0700

    prints the size of various on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - main function
- *
+ * main - A program that prints the size of various computer types
  * Return: 0 when successful
  */
 
- int main(void)
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char);
 	printf("Size of an int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", sizeof(float));
-	return(0);
+return(0);
 }
