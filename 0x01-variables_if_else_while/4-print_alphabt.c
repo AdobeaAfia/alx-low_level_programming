73cd815f274beabfdcb708632379e849b6953ca0
AdobeaAfia <adobea_afia@yahoo.com>
2023-10-06 23:49:22 -0700

    C variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * main - Prog Entry point
- * Reurn: Always 0 (Success)
+ * main - Prints the alphabet without q and e.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char c;
+	int i;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
 		}
+	}
 	putchar('\n');
 	return (0);
 }
