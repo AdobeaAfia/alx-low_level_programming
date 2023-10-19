81a3f1437500c17b95f2518958e524bac25e9f72
AdobeaAfia <adobea_afia@yahoo.com>
2023-10-19 15:31:46 -0700

    a function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,27 @@
 #include "main.h"
-
 /**
- * _strlen - finds the length of string
- * @s: string to be counted
- * Return: length of string
+ * _strncpy - copy and find the length of a string
+ * @dest: input value of string to be counted
+ * @src: input value of string to be counted
+ * @n: input value of string to be counted
+ * Return: dest
  */
 
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
-	int count = 0;
 
-	for (a = 0 ; s[a] != '\0' ; a++)
-		count++;
+	a = 0;
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
 
-	return (count);
+	return (dest);
 }
