fe01f95b8ff5633e6f9b5ccb91d72e4933206b77
AdobeaAfia <adobea_afia@yahoo.com>
2023-11-06 12:41:44 -0800

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a desired dog's basic information
+ * @name: First desired member
+ * @age: Second desired member
+ * @owner: Third desired member
+ * Description: Give full description of desired dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
