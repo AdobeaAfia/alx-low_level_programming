78a101c758353bc5514cb0d1853dac62b9a1930a
AdobeaAfia <adobea_afia@yahoo.com>
2024-01-17 15:07:42 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,55 @@
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: successful when subtracted
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: Sucessful multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - module two integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * mod - module two integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
